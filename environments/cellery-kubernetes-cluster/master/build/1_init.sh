5eb8816109134b8f1b22644eb7b6eeaede97bbab
Miraj Abeysekara <miraj@wso2.com>
2019-11-16 11:03:45 +0530

    Update master image to v0.5.0 release

--- environments/cellery-kubernetes-cluster/master/build/1_init.sh
+++ environments/cellery-kubernetes-cluster/master/build/1_init.sh
@@ -3,15 +3,15 @@
 TEMP_DIR=/tmp
 
 install_ballerina(){
-    wget --directory-prefix=${TEMP_DIR} https://product-dist.ballerina.io/downloads/0.991.0/ballerina-linux-installer-x64-0.991.0.deb
-    sudo dpkg -i ${TEMP_DIR}/ballerina-linux-installer-x64-0.991.0.deb
-    sudo rm ${TEMP_DIR}/ballerina-linux-installer-x64-0.991.0.deb
+    wget --directory-prefix=${TEMP_DIR} https://product-dist.ballerina.io/downloads/1.0.3/ballerina-linux-installer-x64-1.0.3.deb
+    sudo dpkg -i ${TEMP_DIR}/ballerina-linux-installer-x64-1.0.3.deb
+    sudo rm ${TEMP_DIR}/ballerina-linux-installer-x64-1.0.3.deb
 }
 
 install_cellery(){
-    wget --directory-prefix=${TEMP_DIR} https://github.com/wso2-cellery/sdk/releases/download/v0.4.0/cellery-ubuntu-x64-0.4.0.deb
-    sudo dpkg -i ${TEMP_DIR}/cellery-ubuntu-x64-0.4.0.deb
-    sudo rm ${TEMP_DIR}/cellery-ubuntu-x64-0.4.0.deb
+    wget --directory-prefix=${TEMP_DIR} https://github.com/wso2-cellery/sdk/releases/download/v0.5.0/cellery-ubuntu-x64-0.5.0.deb
+    sudo dpkg -i ${TEMP_DIR}/cellery-ubuntu-x64-0.5.0.deb
+    sudo rm ${TEMP_DIR}/cellery-ubuntu-x64-0.5.0.deb
 }
 
 
